2da660613a257a0a7b932bb7364bfca4a0ade9ff
natieee <104625800+natieee@users.noreply.github.com>
2025-07-21 01:50:37 +0000

    basta yon

---
+++ hello.cs
@@ -0,0 +1,61 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello world gago");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  
+
+
+  
+
+
+
+
+
+
+
+
+
+
+  
